122cc94642c68be678cdd54bbc9b9992d75aba95
Domenico Luciani <domenicoleoneluciani@gmail.com>
2025-09-20 04:04:56 +0200

    rogue tu puta madre

--- assets/js/main.js
+++ assets/js/main.js
@@ -25,4 +25,43 @@
     }
   });
 
+  const prettifyLang = function (lang) {
+    if (!lang) return '';
+    const normalized = lang.toLowerCase();
+    const map = {
+      js: 'JS', javascript: 'JS', jsx: 'JSX',
+      ts: 'TS', typescript: 'TS', tsx: 'TSX',
+      go: 'Go', golang: 'Go',
+      py: 'Python', python: 'Python',
+      rb: 'Ruby', ruby: 'Ruby',
+      rs: 'Rust', rust: 'Rust',
+      java: 'Java', kotlin: 'Kotlin', swift: 'Swift',
+      php: 'PHP',
+      c: 'C', cpp: 'C++', cxx: 'C++', 'c++': 'C++',
+      cs: 'C#', csharp: 'C#', 'c#': 'C#',
+      objc: 'Obj-C', objectivec: 'Obj-C',
+      scala: 'Scala',
+      bash: 'Bash', sh: 'Shell', shell: 'Shell', zsh: 'Zsh', fish: 'Fish',
+      sql: 'SQL', yaml: 'YAML', yml: 'YAML', json: 'JSON', toml: 'TOML', ini: 'INI',
+      dockerfile: 'Docker', make: 'Make', makefile: 'Make',
+      diff: 'Diff', html: 'HTML', xml: 'XML', css: 'CSS', scss: 'SCSS', sass: 'Sass',
+      plaintext: 'Text', text: 'Text'
+    };
+    if (map[normalized]) return map[normalized];
+    return normalized.replace(/\b\w/g, function (ch) {
+      return ch.toUpperCase();
+    }).replace(/-/g, ' ');
+  };
+
+  document.querySelectorAll('.highlighter-rouge').forEach(function (block) {
+    const pre = block.querySelector('pre');
+    let match = pre && pre.className && pre.className.match(/language-([\w#+-]+)/i);
+    if (!match && block.className) {
+      match = block.className.match(/language-([\w#+-]+)/i);
+    }
+    if (match && match[1]) {
+      block.setAttribute('data-lang', prettifyLang(match[1]));
+    }
+  });
+
 })();
